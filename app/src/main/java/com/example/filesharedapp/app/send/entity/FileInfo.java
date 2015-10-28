656b1f5aee06c5cc3cb001cadda0a3247793bec0
yanling <1054974640@qq.com>
2015-10-28 17:25:53 +0800

    添加传输信息实体

---
+++ app/src/main/java/com/example/filesharedapp/app/send/entity/FileInfo.java
@@ -0,0 +1,67 @@
+package com.example.filesharedapp.app.send.entity;
+
+import java.io.Serializable;
+
+/**
+ * 传输文件信息实体
+ * Created by yanling on 2015/10/28.
+ */
+public class FileInfo implements Serializable{
+
+    private static final long serialVersionUID = 6905472348213952006L;
+
+    //文件名
+    private String fileName;
+
+    //文件所在路径
+    private String path;
+
+    //文件大小
+    private long size;
+
+    //文件类型（1:app, 2:图片，3：视频，4：音乐，5：其他）
+    private int type;
+
+    //文件的md5值，主要用于校验文件的完整性
+    private String md5;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
+
+    public void setMd5(String md5) {
+        this.md5 = md5;
+    }
+}
---
+++ app/src/main/java/com/example/filesharedapp/app/send/entity/QrcodeInfo.java
@@ -0,0 +1,35 @@
+package com.example.filesharedapp.app.send.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 传输信息实体，包括socket连接的端口号，传输文件信息
+ * Created by yanling on 2015/10/28.
+ */
+public class QrcodeInfo implements Serializable{
+
+    private static final long serialVersionUID = 3651614906726502582L;
+
+    //socket连接的端口号
+    private int hostPort;
+
+    //文件信息列表，定义list方便后期扩展发送多个文件（注意文件不能太多，可能会超出二维码存储的容量）
+    private List<FileInfo> files;
+
+    public int getHostPort() {
+        return hostPort;
+    }
+
+    public void setHostPort(int hostPort) {
+        this.hostPort = hostPort;
+    }
+
+    public List<FileInfo> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<FileInfo> files) {
+        this.files = files;
+    }
+}
